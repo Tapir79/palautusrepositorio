8b2dad946e1627303086efc78f5affd2ed2e9eca
Tapir79 <saara.m.pakarinen@gmail.com>
2025-11-02 23:05:39 +0200

    Add tulo.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,6 +3,7 @@
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo 
 
 logger("aloitetaan ohjelma") # muutos mainissa
 
@@ -10,6 +11,7 @@ x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"Lukujen {x} ja {y} summa on {x} + {y} = {summa(x, y)}")  # yhdistelmämuutos bugikorjaus-branchissa ja mainissa
 print(f"Lukujen {x} ja {y} erotus on {x} - {y} = {erotus(x, y)}")  # yhdistelmämuutos bugikorjaus-branchissa ja mainissa
+print(f"{x} * {y} = {tulo(x, y)}")
 
 logger("lopetetaan ohjelma")
 print("goodbye!") # lisäys bugikorjaus-branchissa
