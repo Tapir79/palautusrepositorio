6628b1bc153140c2a3c86776892bbe6dd9ae4307
Tapir79 <saara.m.pakarinen@gmail.com>
2025-11-23 21:53:24 +0200

    Add week6 ex1

---
+++ dokumentaatio/tehtavat6.md
@@ -0,0 +1,6 @@
+## viikko 6 
+
+### Tehtävä 1 - Laskin ja komento-oliot
+- [X] Kopioi viikko6/laskin     
+- [X] Asenna riippuvuudet ja testaa laskinta   
+- [X] Refaktoroi if-hässäkkä     
--- viikko6/laskin/src/kayttoliittyma.py
+++ viikko6/laskin/src/kayttoliittyma.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from tkinter import ttk, constants, StringVar
 
+from sovelluslogiikka import Sovelluslogiikka
+
 
 class Komento(Enum):
     SUMMA = 1
@@ -13,6 +15,8 @@ class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+        self._komennot = {}
+        
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -47,6 +51,13 @@ class Kayttoliittyma:
             command=lambda: self._suorita_komento(Komento.KUMOA)
         )
 
+        self._komennot = {
+            Komento.SUMMA: Summa(self._sovelluslogiikka, self._lue_syote),
+            Komento.EROTUS: Erotus(self._sovelluslogiikka, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(self._sovelluslogiikka),
+            Komento.KUMOA: Kumoa()
+        }
+
         tulos_teksti.grid(columnspan=4)
         self._syote_kentta.grid(columnspan=4, sticky=(constants.E, constants.W))
         summa_painike.grid(row=2, column=0)
@@ -54,22 +65,13 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
+        # haetaan dict:istä oikea komento
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
 
         self._kumoa_painike["state"] = constants.NORMAL
 
@@ -80,3 +82,38 @@ class Kayttoliittyma:
 
         self._syote_kentta.delete(0, constants.END)
         self._arvo_var.set(self._sovelluslogiikka.arvo())
+
+    
+# --- Command luokat --- #
+
+class Summa:
+    def __init__(self, sovelluslogiikka:Sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        arvo = int(self._lue_syote())
+        self._sovelluslogiikka.plus(arvo)
+
+class Erotus:
+    def __init__(self, sovelluslogiikka:Sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        arvo = int(self._lue_syote())
+        self._sovelluslogiikka.miinus(arvo)
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka:Sovelluslogiikka):
+        self._sovelluslogiikka = sovelluslogiikka
+
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+class Kumoa:
+    def __init__(self):
+        pass
+
+    def suorita(self):
+        pass
