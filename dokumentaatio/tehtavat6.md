3aa12f7096a99d1157ad8024ccd244727fb0a4aa
Tapir79 <saara.m.pakarinen@gmail.com>
2025-11-28 09:38:58 +0200

    Add week6 ex4

--- dokumentaatio/tehtavat6.md
+++ dokumentaatio/tehtavat6.md
@@ -3,6 +3,17 @@
 ### Tehtävä 1 - Laskin ja komento-oliot
 - [X] Kopioi viikko6/laskin     
 - [X] Asenna riippuvuudet ja testaa laskinta   
-- [X] Refaktoroi if-hässäkkä     
+- [X] Refaktoroi if-hässäkkä   
+
+### Tehtävä 2 - kumoa toiminto 
 - [X] Toteuta laskimeen kumoa-toiminto
-- [X] Bonus: mielivaltainen määrä peräkkäisiä kumoamisia     
+- [X] Bonus: mielivaltainen määrä peräkkäisiä kumoamisia   
+
+### Tehtävä 3 - Kyselykieli NHL-tilastoihin    
+- [X] Toteuta strategy pattern   All   
+- [X] Toteuta strategy pattern   HasFewerThan     
+- [X] Toteuta strategy pattern   Not         
+
+### Tehtävä 4 - Kyselykieli NHL-tilastoihin, osa 2
+- [X] Toteuta strategy pattern   Or     
+- [X] Varmista, että kyselyt palauttavat oikeat pelaajat      
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,4 +1,5 @@
 class And:
+    """Kaikki ehdot täyttävä matcher."""
     def __init__(self, *matchers):
         self._matchers = matchers
 
@@ -9,6 +10,17 @@ class And:
 
         return True
 
+class Or:
+    """mikä tahansa ehdoista toteutuu"""
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
 
 class PlaysIn:
     def __init__(self, team):
@@ -27,3 +39,25 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+    
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+    
+class All:
+    def test(self, player):
+        return True
+    
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def test(self, player):
+        return not self._matcher.test(player)
+ 
